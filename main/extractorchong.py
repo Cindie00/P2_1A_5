eb4e2a9bf4fd9dc1178052371a2fe464ee20d16b
Cindie00 <74539189+Cindie00@users.noreply.github.com>
2021-05-06 15:59:38 +0200

    Create extractorchong.py

---
+++ main/extractorchong.py
@@ -0,0 +1,38 @@
+#############################
+#
+# Extracteur de donnés de Chong de l'exercice 2. ce qui équivaut à la Question 4 sur le site
+#
+#########################################################################################################@
+
+import sqlite3
+with sqlite3.connect("Base2.db") as conn:
+    cursor=conn.cursor()
+    data=list(cursor.execute("SELECT mere_id, date FROM velages"))
+    
+def Velage():
+    '''
+    pre:/
+    post: renvoie un dictionnaire avec le nombre de velages par année
+    '''
+    dico={}
+    annee={}
+    for x in data:       # on crée 2 dictionnaires avec le nombre de velages par années
+        if dico.get(x[0],None) == None:
+            dico[x[0]]=1
+        else:
+            dico[x[0]]+=1
+        if annee.get(x[1][-4:],None) == None:
+            annee[x[1][-4:]]={dico[x[0]]:1}
+        
+        else:
+            if annee[x[1][-4:]].get(dico[x[0]],None) == None:
+                annee[x[1][-4:]][dico[x[0]]]=1
+            else:
+                annee[x[1][-4:]][dico[x[0]]]+=1
+
+    for a in annee:        #on récupère les donnés pour chaque année
+        for i in range(1,8):
+            if annee[a].get(i,None) == None:
+                annee[a][i]=0
+        
+     return annee #on renvoie le dictionnaire avec les données du nombre de velages par an
